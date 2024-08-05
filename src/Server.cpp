758ec700341eb5e45a0d5d35032dd09b961df76f
Stephen Horne <code@stephenhorne.com>
2024-08-05 15:22:31 -0400

    Remove extra logic, check inputs for potential overruns

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <regex>
 
 bool match_pattern(const std::string& input, const std::string& pattern) {
@@ -18,28 +19,27 @@ int main(int argc, char* argv[]) {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    if (argc != 3) {
-        std::cerr << "Expected two arguments" << std::endl;
+    if (argc != 3 || std::strcmp("-E", argv[1])) {
+        std::cerr << "Usage: " << argv[0] << " -E PATTERN" << std::endl;
         return 1;
     }
 
-    std::string flag = argv[1];
-    std::string pattern = argv[2];
+    std::string pattern;
 
-    if (flag != "-E") {
-        std::cerr << "Expected first argument to be '-E' flag" << std::endl;
+    if (std::strlen(argv[2]) > pattern.max_size()) {
+        std::cerr << "Pattern is too long. Must be under " << pattern.max_size() << " bytes." << std::endl;
         return 1;
     }
+
+    pattern = argv[2];
 
     std::string input;
+
+    // overruns are prevented automatically here with an internal call of input.max_size()
     std::getline(std::cin, input);
 
     try {
-        if (match_pattern(input, pattern)) {
-            return 0;
-        }
-
-        return 1;
+        return !match_pattern(input, pattern); // exit code 0 if true, else 1
     } catch (const std::runtime_error& e) {
         std::cerr << e.what() << std::endl;
         return 1;
